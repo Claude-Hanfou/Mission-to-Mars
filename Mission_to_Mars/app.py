c4514bc243dfad7a21b5378089f2401e45b773fb
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2021-01-28 17:04:28 -0600

    update

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -20,6 +20,7 @@ def index():
 
 @app.route("/scrape")
 def scrape():
+    
     mars_data = scrape_mars.scrape_info()
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
